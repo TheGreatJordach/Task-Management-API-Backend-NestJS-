f2ef8eb7aafad65b27ac5c3cdc6bed491b263105
Jordach Makaya H.M <4thinkai@gmail.com>
2024-09-18 02:48:03 +0100

    adding env validation

--- src/configuration/env.validation.ts
+++ src/configuration/env.validation.ts
@@ -1,2 +1,43 @@
+import { IsNotEmptyString, IsPositiveInteger } from "../common/decorators/validator.decotrator";
+import { plainToInstance } from "class-transformer";
+import { validateSync } from "class-validator";
 
-export class EnvironmentVariables {}
+export class EnvironmentVariables {
+
+  // String
+  @IsNotEmptyString()
+  NODE_ENV:string
+  @IsNotEmptyString()
+  DATASOURCE_USERNAME:string
+  @IsNotEmptyString()
+  DATASOURCE_PASSWORD:string
+  @IsNotEmptyString()
+  DATASOURCE_DATABASE:string
+  @IsNotEmptyString()
+  DATASOURCE_HOST:string
+
+// Number
+  @IsPositiveInteger()
+  APP_PORT:number
+  @IsPositiveInteger()
+  DATASOURCE_PORT:number
+  @IsPositiveInteger()
+  DATABASE_URL:number
+}
+
+
+export function envValidator(providedConfig:Record<string, unknown> ){
+  const validatedConfig = plainToInstance(EnvironmentVariables,providedConfig,{
+    enableCircularCheck:true
+  })
+
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties:false
+  })
+
+  if (errors.length > 0) {
+    throw new Error(`${errors.length} not valid environment variable found`)
+  }
+
+  return validatedConfig;
+}
